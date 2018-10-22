5aafa72569acb38d053cdd01a576072892273dec
Omer Prizner <omer.prizner@gmail.com>
2018-10-22 21:48:19 +0300

    use different free api.. that supports chaning base to USD..

--- packages/server/src/rates-manager/index.ts
+++ packages/server/src/rates-manager/index.ts
@@ -4,7 +4,7 @@ import { IRates } from 'src/rates-manager/typings';
 export const ratesManager = {
   get(): Promise<IRates> {
     return (
-      fetch('http://data.fixer.io/api/latest?access_key=780070b33bed84b5003e036aa4704e5f&format=1')
+      fetch('https://ratesapi.io/api/latest?base=USD')
         .then(response => response.json())
         .then(responseData => responseData.rates)
     );
