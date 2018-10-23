4386ff819c301bfc40286569167d2e13aa74bc4b
Omer Prizner <omer.prizner@gmail.com>
2018-10-23 22:01:17 +0300

    Added a cute loader

--- packages/client/src/store/index.ts
+++ packages/client/src/store/index.ts
@@ -1,14 +1,36 @@
 import { RouterStore } from 'client/src/store/router';
 import { HomeStore } from 'client/src/store/home';
+import { runInAction, computed, observable } from 'mobx';
 
 export class Store {
   public readonly router: RouterStore;
 
   public readonly home: HomeStore;
 
+  @observable
+  private ready: boolean;
+
   constructor() {
     this.router = new RouterStore();
     this.home = new HomeStore();
+    this.ready = false;
+    this.getReady();
+  }
+
+  private getReady() {
+    // Getting ready. #9
+    setTimeout(
+      () => {
+        runInAction(() => {
+          this.ready = true;
+        });
+      },
+      3000,
+    );
+  }
+
+  @computed get isReady() {
+    return this.router.isStarted() && this.ready;
   }
 }
 
--- packages/client/src/view/container/router.tsx
+++ packages/client/src/view/container/router.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import CircularProgress from '@material-ui/core/CircularProgress';
 import { MobxHoc } from 'src/view/provider/mobx-react/hoc';
 import { ERouteType } from 'client/src/config/router/type';
 import { routeComponent } from 'client/src/config/router/route-component';
@@ -10,12 +11,30 @@ interface IRouterProps {
 
 export const Router = MobxHoc<IRouterProps>(
   ({ store }) => ({
-    isReady: () => store.router.isStarted(),
+    isReady: () => store.isReady,
     selectedRouteType: () => store.router.routeType,
   }),
 )(({ isReady, selectedRouteType }) => {
   if (!isReady()) {
-    return null;
+    return (
+      <div // Can be extracted to a neat component, but it is a simple loader so..
+        style={{
+          position: 'absolute',
+          top: 0,
+          right: 0,
+          left: 0,
+          bottom: 0,
+          display: 'flex',
+          justifyContent: 'center',
+          alignItems: 'center',
+        }}
+      >
+        <CircularProgress
+          color="primary"
+          size={100}
+        />
+      </div>
+    );
   }
 
   const RouteComponent = routeComponent[selectedRouteType()];
